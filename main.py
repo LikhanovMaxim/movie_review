0d42edf1150c4bfacf449a34bb980aa9f2922141
Likhanov Maxim <Maksim_Likhanov@epam.com>
2019-01-10 04:41:27 +0300

    remove unused comments

--- main.py
+++ main.py
@@ -8,22 +8,16 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 
 
-def prepare_my():
-    # Do it once:
+def prepare_data():
     prepare.prepare_data()
     print("Start reading")
-    # f = open(DIRECTORY + BAG_OF_WORDS, "r")
-    # contents = f.read()
-    # print(contents)
-    # print(contents.toarray)
     [matrix, stars, vocab] = prepare.take_bag_of_words()
+    prepare.print_info_matrix(matrix, vocab)
     print(stars)
     print(stars.shape)
-    # prepare.print_info_matrix(matrix, vocab)
 
 
 def change(train_y, size):
-    # TODO
     train_y = [int(i) for i in train_y]
     train_y = np.asarray(train_y)
     train_y = np.reshape(train_y, (size, 1))
@@ -33,37 +27,17 @@ def change(train_y, size):
     return train_y
 
 
-def run_my():
+def run():
     [matrix, stars, vocab] = prepare.take_bag_of_words()
     prepare.print_info_matrix(matrix, vocab)
-    # stars = column(matrix, 0)
-    print("stars")
-    # print(stars)
     num_words = len(matrix[0])
-    (training_data, training_targets), (testing_data, testing_targets) = imdb.load_data(num_words=num_words)
-    # data = np.concatenate((training_data, testing_data), axis=0)
-
-    # targets = np.concatenate((training_targets, testing_targets), axis=0)
-    # data = vectorize(data, num_words)
-    # Также нужно выполнить преобразование переменных в тип float.
-    # stars = np.array(stars).astype("float32")
-    # Разделим датасет на обучающий и тестировочный наборы.
-    # Обучающий набор будет состоять из 40 000 обзоров,
-    # тестировочный — из 10 000.
     size_rows = len(matrix)
     size_tests_data = int(size_rows / 4)
     size_tests_data_del_2 = int(size_tests_data / 2)
-    print("size")
-    print(size_rows)
-    print(size_tests_data)
-    # train_x = matrix[size_tests_data:]
     size_before_smth = (size_rows - size_tests_data_del_2)
     train_x = matrix[size_tests_data_del_2:size_before_smth]
-    # train_y = stars[size_tests_data:]
     train_y = stars[size_tests_data_del_2:size_before_smth]
-    # test_x = matrix[:size_tests_data]
     test_x = np.concatenate((matrix[0:size_tests_data_del_2], matrix[size_before_smth:size_rows]))
-    # test_y = stars[:size_tests_data]
     test_y = np.concatenate((stars[0:size_tests_data_del_2], stars[size_before_smth:size_rows]))
     # Последовательная модель
     model = models.Sequential()
@@ -86,22 +60,12 @@ def run_my():
         metrics=["accuracy"]
     )
     # Convert labels to categorical one-hot encoding
-
     train_y = change(train_y, size_rows - size_tests_data)
-    # np.reshape(test_y, (50, 1))
-    # labels = np.random.randint(10, size=(150, 1))
-    # one_hot_labels_2 = to_categorical(test_y, num_classes=8)
-
     one_hot_labels = to_categorical(train_y, num_classes=11)
-    # <class 'tuple'>: (1000, 1) ndarray
-    # Convert labels to categorical one-hot encoding
-    # one_hot_labels = to_categorical(labels, num_classes=10)
-    # <class 'tuple'>: (1000, 1) ndarray
-    # Convert labels to categorical one-hot encoding
+
     test_y = change(test_y, size_tests_data)
-    # labels_2 = np.random.randint(10, size=(50, 1))
-    # one_hot_labels_2 = to_categorical(labels_2, num_classes=10)
     one_hot_labels_2 = to_categorical(test_y, num_classes=11)
+
     results = model.fit(
         train_x, one_hot_labels,
         epochs=5,  # 10, 15, 20 maximum
@@ -112,5 +76,4 @@ def run_my():
 
 
 if __name__ == '__main__':
-    # prepare_my()
-    run_my()
+    run()
--- prepare_train_data.py
+++ prepare_train_data.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
@@ -32,7 +31,6 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 
 REVIEW = 1
-# REVIEW = "review"
 
 FILE = "testData.tsv"
 DATA_FOR_LEARNING = "labeledTrainData_simple.tsv"
@@ -40,7 +38,6 @@ DATA_FOR_LEARNING_FULL = "labeledTrainData.tsv"
 
 
 def normalization(blank_review, remove_stop_words=False, need_to_lemm=False):
-    # print(blank_review)
     # Функция преобразования документа в список слов
     # Необязательно удалять все, кроме словарных слов.
     #
@@ -63,13 +60,9 @@ def normalization(blank_review, remove_stop_words=False, need_to_lemm=False):
     if need_to_lemm:
         wnl = WordNetLemmatizer()
         words = [wnl.lemmatize(w) for w in words]
-        # >>> wnl = WordNetLemmatizer()
-        # >>> print(wnl.lemmatize('cats'))
-        # cat
     #
     # 6. Присоединим слова к одной строке, разделенной пробелом, и вернем результат.
     return " ".join(words)
-    # return words
 
 
 def normalization_text(raw_data):
@@ -79,7 +72,6 @@ def normalization_text(raw_data):
     print("Cleaning and parsing the training set movie reviews... \n")
     net_initial_reviews = []
     for i in range(0, number_of_reviews):
-        # Если индекс равномерно делится на 1000, распечатайте сообщение
         if (i + 1) % 1000 == 0:
             print("Review %d of %d\n" % (i + 1, number_of_reviews))
         cleaned = normalization(raw_data[REVIEW][i], True, True)
@@ -104,7 +96,6 @@ def normalization_text_matrix(raw_data):
     return net_initial_reviews
 
 
-# It is not used
 def review_of_sent(blank_review, tokenizer, remove_stop_words=False, need_to_lemm=False):
     # Функция для разделения обзора(отзыва) на разобранные предложения.
     # Возвращает список предложений, где каждое предложение представляет собой список слов
@@ -130,36 +121,8 @@ def unzip():
         print(data[item])
 
 
-def testing(test):
-    wnl = WordNetLemmatizer()
-    # TODO check it. is it OK?
-    words = ['got', 'watched', 'watches']
-    words = [wnl.lemmatize(w) for w in words]
-    print(words)
-    # print(test.shape)
-    # print(test)
-    # print(test["sentiment"])
-    # print(test["id"])
-    # print(test["review"])
-    # check = test[REVIEW][0]
-    # print(check)
-    # print(normalization(check))
-    # print(normalization(check, True))
-    # print("rev_of_ws")
-    # # print(normalization(check))
-    # print(normalization(check, True, True))
-
-
 def write_to_file(train_data, file_name):
-    # print(np.matrix(train_data))
     np.save(file_name, train_data)
-    # output = pd.DataFrame(data={"id": train_data[0], "sentiment": train_data[1]})
-    # df.to_csv(file_name, sep='\t')
-    # # Скопируем результаты в таблицу данных pandas с колонкой «id» и
-    # # столбец «настроение»
-    # output = pd.DataFrame(data={"id": train_data["id"], "sentiment": result})
-    # # Используем pandas для записи выходного файла, разделенного запятыми
-    # output.to_csv("Bag_of_Words_model.csv", index=False, quoting=3)
 
 
 def bag_of_words(norm_text):
@@ -169,15 +132,8 @@ def bag_of_words(norm_text):
                                  tokenizer=None,
                                  preprocessor=None,
                                  stop_words=None)
-    # TODO is it needed?
-    # max_features=MAX_FEATURES)
     train_data = vectorizer.fit_transform(norm_text)
-    # print(train_data)
-    # Numpy массивы легко работают, поэтому преобразуем получившийся результат в массив
     train_data = train_data.toarray()
-    # print("toarray")
-    # print(train_data.shape)
-    # Посмотрим на слова в словаре
     vocab = vectorizer.get_feature_names()
     return train_data, vocab
 
@@ -198,67 +154,36 @@ def tfidf_method(norm_text):
                                  stop_words=None,
                                  max_features=MAX_FEATURES)
     matrix = vectorizer.fit_transform(norm_text)
-    # float_formatter = lambda x: "%.2f" % x
-    # np.set_printoptions(formatter={'float_kind': float_formatter})
     return matrix.toarray(), vectorizer.get_feature_names()
 
 
 def print_info_matrix(matrix, vocab):
     print(matrix.shape)
-    # print(np.matrix(matrix))
     print(vocab[0:100])
     print_often_words(matrix, vocab)
 
 
 def read_directory(directory):
-    # f = open(directory + "0_10.txt", "r")
-    # print(f.read())
     only_files = [f for f in listdir(directory) if isfile(join(directory, f))]
-    # only_files = ['0_10.txt']
-    # row = []
-    # matrix = []
     i = 0
     matrix = ["0", "a"]  # TODO remove it
-    # size = only_files.index()
     print("start handling files")
     for file in only_files:
-        # print(file)
         f = open(directory + file, "r", encoding="utf-8")
         text = f.read()
         star = re.search(PATTERN, file).group(1)
         row = [star, text]
-        # print(row)
-        # matrix.append(row)
-        # TODO Why is is so slow? np.vstack?
         matrix = np.vstack((matrix, row))
-        # matrix = np.concatenate((matrix, row))
         i = i + 1
         if (i + 1) % 100 == 0:
             print("Files %d" % (i + 1))
-            # break
-        if (i + 1) % 1000 == 0:
-            break
     return matrix
-    # matrix like this = [id star review]
 
 
 def take_train_data():
     pos = read_directory(DIRECTORY + "pos\\")
     neg = read_directory(DIRECTORY + "neg\\")
     return np.vstack((pos, neg))
-    # first = ["8",
-    #          "With all this stuff going down at the moment with MJ i've started listening to his music, watching the odd documentary here and there, watched The Wiz and watched Moonwalker again. Maybe i just want to get a certain insight into this guy who i thought was really cool in the eighties just to maybe make up my mind whether he is guilty or innocent. Moonwalker is part biography, part feature film which i remember going to see at the cinema when it was originally released. Some of it has subtle messages about MJ's feeling towards the press and also the obvious message of drugs are bad m'kay.<br /><br />Visually impressive but of course this is all about Michael Jackson so unless you remotely like MJ in anyway then you are going to hate this and find it boring. Some may call MJ an egotist for consenting to the making of this movie BUT MJ and most of his fans would say that he made it for the fans which if true is really nice of him.<br /><br />The actual feature film bit when it finally starts is only on for 20 minutes or so excluding the Smooth Criminal sequence and Joe Pesci is convincing as a psychopathic all powerful drug lord. Why he wants MJ dead so bad is beyond me. Because MJ overheard his plans? Nah, Joe Pesci's character ranted that he wanted people to know it is he who is supplying drugs etc so i dunno, maybe he just hates MJ's music.<br /><br />Lots of cool things in this like MJ turning into a car and a robot and the whole Speed Demon sequence. Also, the director must have had the patience of a saint when it came to filming the kiddy Bad sequence as usually directors hate working with one kid let alone a whole bunch of them performing a complex dance scene.<br /><br />Bottom line, this movie is for people who like MJ on one level or another (which i think is most people). If not, then stay away. It does try and give off a wholesome message and ironically MJ's bestest buddy in this movie is a girl! Michael Jackson is truly one of the most talented people ever to grace this planet but is he guilty? Well, with all the attention i've gave this subject....hmmm well i don't know because people can be different behind closed doors, i know this for a fact. He is either an extremely nice but stupid guy or one of the most sickest liars. I hope he is not the latter."]
-    # second = ["3",
-    #           "The film starts with a manager (Nicholas Bell) giving welcome investors (Robert Carradine) to Primal Park . A secret project mutating a primal animal using fossilized DNA, like ¨Jurassik Park¨, and some scientists resurrect one of nature's most fearsome predators, the Sabretooth tiger or Smilodon . Scientific ambition turns deadly, however, and when the high voltage fence is opened the creature escape and begins savagely stalking its prey - the human visitors , tourists and scientific.Meanwhile some youngsters enter in the restricted area of the security center and are attacked by a pack of large pre-historical animals which are deadlier and bigger . In addition , a security agent (Stacy Haiduk) and her mate (Brian Wimmer) fight hardly against the carnivorous Smilodons. The Sabretooths, themselves , of course, are the real star stars and they are astounding terrifyingly though not convincing. The giant animals savagely are stalking its prey and the group run afoul and fight against one nature's most fearsome predators. Furthermore a third Sabretooth more dangerous and slow stalks its victims.<br /><br />The movie delivers the goods with lots of blood and gore as beheading, hair-raising chills,full of scares when the Sabretooths appear with mediocre special effects.The story provides exciting and stirring entertainment but it results to be quite boring .The giant animals are majority made by computer generator and seem totally lousy .Middling performances though the players reacting appropriately to becoming food.Actors give vigorously physical performances dodging the beasts ,running,bound and leaps or dangling over walls . And it packs a ridiculous final deadly scene. No for small kids by realistic,gory and violent attack scenes . Other films about Sabretooths or Smilodon are the following : ¨Sabretooth(2002)¨by James R Hickox with Vanessa Angel, David Keith and John Rhys Davies and the much better ¨10.000 BC(2006)¨ by Roland Emmerich with with Steven Strait, Cliff Curtis and Camilla Belle. This motion picture filled with bloody moments is badly directed by George Miller and with no originality because takes too many elements from previous films. Miller is an Australian director usually working for television (Tidal wave, Journey to the center of the earth, and many others) and occasionally for cinema ( The man from Snowy river, Zeus and Roxanne,Robinson Crusoe ). Rating : Below average, bottom of barrel."]
-    # third = ["7", "asdasdasd asd "]
-    # first = ["8", "3"]
-    # first = ["id", "review"]
-    # second = ["With all this stuff going down at the moment with MJ i've started listening to his music, watching the odd documentary here and there, watched The Wiz and watched Moonwalker again. Maybe i just want to get a certain insight into this guy who i thought was really cool in the eighties just to maybe make up my mind whether he is guilty or innocent. Moonwalker is part biography, part feature film which i remember going to see at the cinema when it was originally released. Some of it has subtle messages about MJ's feeling towards the press and also the obvious message of drugs are bad m'kay.<br /><br />Visually impressive but of course this is all about Michael Jackson so unless you remotely like MJ in anyway then you are going to hate this and find it boring. Some may call MJ an egotist for consenting to the making of this movie BUT MJ and most of his fans would say that he made it for the fans which if true is really nice of him.<br /><br />The actual feature film bit when it finally starts is only on for 20 minutes or so excluding the Smooth Criminal sequence and Joe Pesci is convincing as a psychopathic all powerful drug lord. Why he wants MJ dead so bad is beyond me. Because MJ overheard his plans? Nah, Joe Pesci's character ranted that he wanted people to know it is he who is supplying drugs etc so i dunno, maybe he just hates MJ's music.<br /><br />Lots of cool things in this like MJ turning into a car and a robot and the whole Speed Demon sequence. Also, the director must have had the patience of a saint when it came to filming the kiddy Bad sequence as usually directors hate working with one kid let alone a whole bunch of them performing a complex dance scene.<br /><br />Bottom line, this movie is for people who like MJ on one level or another (which i think is most people). If not, then stay away. It does try and give off a wholesome message and ironically MJ's bestest buddy in this movie is a girl! Michael Jackson is truly one of the most talented people ever to grace this planet but is he guilty? Well, with all the attention i've gave this subject....hmmm well i don't know because people can be different behind closed doors, i know this for a fact. He is either an extremely nice but stupid guy or one of the most sickest liars. I hope he is not the latter.", "The film starts with a manager (Nicholas Bell) giving welcome investors (Robert Carradine) to Primal Park . A secret project mutating a primal animal using fossilized DNA, like ¨Jurassik Park¨, and some scientists resurrect one of nature's most fearsome predators, the Sabretooth tiger or Smilodon . Scientific ambition turns deadly, however, and when the high voltage fence is opened the creature escape and begins savagely stalking its prey - the human visitors , tourists and scientific.Meanwhile some youngsters enter in the restricted area of the security center and are attacked by a pack of large pre-historical animals which are deadlier and bigger . In addition , a security agent (Stacy Haiduk) and her mate (Brian Wimmer) fight hardly against the carnivorous Smilodons. The Sabretooths, themselves , of course, are the real star stars and they are astounding terrifyingly though not convincing. The giant animals savagely are stalking its prey and the group run afoul and fight against one nature's most fearsome predators. Furthermore a third Sabretooth more dangerous and slow stalks its victims.<br /><br />The movie delivers the goods with lots of blood and gore as beheading, hair-raising chills,full of scares when the Sabretooths appear with mediocre special effects.The story provides exciting and stirring entertainment but it results to be quite boring .The giant animals are majority made by computer generator and seem totally lousy .Middling performances though the players reacting appropriately to becoming food.Actors give vigorously physical performances dodging the beasts ,running,bound and leaps or dangling over walls . And it packs a ridiculous final deadly scene. No for small kids by realistic,gory and violent attack scenes . Other films about Sabretooths or Smilodon are the following : ¨Sabretooth(2002)¨by James R Hickox with Vanessa Angel, David Keith and John Rhys Davies and the much better ¨10.000 BC(2006)¨ by Roland Emmerich with with Steven Strait, Cliff Curtis and Camilla Belle. This motion picture filled with bloody moments is badly directed by George Miller and with no originality because takes too many elements from previous films. Miller is an Australian director usually working for television (Tidal wave, Journey to the center of the earth, and many others) and occasionally for cinema ( The man from Snowy river, Zeus and Roxanne,Robinson Crusoe ). Rating : Below average, bottom of barrel."]
-    # matrix = np.vstack((first, second))
-    # matrix = np.vstack((matrix, third))
-    # return matrix
-
-    # return pd.read_csv(DATA_FOR_LEARNING_FULL, header=0, delimiter="\t", quoting=3)
 
 
 def column(matrix, i):
@@ -267,22 +192,12 @@ def column(matrix, i):
 
 def prepare_data():
     train_data = take_train_data()
-    print(len(train_data))
-    # print(train_data)
-    # testing(train_data)
+
     norm_text = normalization_text_matrix(train_data)
-    # print("Len :")
-    # print(len(norm_text[1]))
-    # print(norm_text[1])
-    # print(len(norm_text[2]))
-    # print(norm_text[2])
-    # print("Tfidf method")
-    # [matrix_tfidf, vocab_tfidf] = tfidf_method(norm_text)
-    # print_info_matrix(matrix_tfidf, vocab_tfidf)
+
     print("\nBag of words")
     [matrix, vocab] = bag_of_words(norm_text)
-    # print_info_matrix(matrix, vocab)
-    # print(matrix[0])
+
     write_to_file(matrix, BAG_OF_WORDS_SMALL)
     write_to_file(column(train_data, 0), BAG_OF_WORDS_STARS_SMALL)
     write_to_file(vocab, BAG_OF_WORDS_VOCAB_SMALL)
